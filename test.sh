dff945ea048ead57804232338ec76b3b64205dc7
Alessandro Carminati <alessandro.carminati@gmail.com>
2025-07-09 21:49:36 +0200

    test scripts
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GOTO=$1
+found=0
+
+for i in $(ls dtbs/* | sort); do
+    if [[ $found -eq 0 ]]; then
+        if [[ "$i" == "$GOTO" ]]; then
+            found=1
+        else
+            echo "skipping $i"
+            continue
+        fi
+    fi
+    echo "$i"
+    ./src/fdt "$i" | xdot -
+done
